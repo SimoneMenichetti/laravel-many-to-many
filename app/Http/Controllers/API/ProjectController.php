d623f3f3e35b398d6a8077bf73548086fe6b3cdc
Simone Menichetti <simon3menichetti95@gmail.com>
2024-10-01 17:05:28 +0200

    initialize function for types & slug type)

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
-use App\Models\Type;
+
 
 class ProjectController extends Controller
 {
